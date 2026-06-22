32aa487904638777d9b7f71cfcdc9893f75a20c5
c-gale <cubylive@gmail.com>
2026-06-22 03:01:38 -0600

    maybe fix?

--- flake.nix
+++ flake.nix
@@ -7,16 +7,25 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        packages.default = pkgs.writeShellScriptBin "subminer" ''
-          exec ${pkgs.appimage-run}/bin/appimage-run ${pkgs.fetchurl {
-            url = "https://github.com/ksyasuda/SubMiner/releases/download/v0.17.0/SubMiner.AppImage";
-            sha256 = "sha256-aEqhxQ0cFxlJ4Tpsh0foIg4O8ZF9QQzVfSULGxPT+iU=";
-          }} "$@"
-        '';      
-        }
-    );
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      subminer = pkgs.appimageTools.wrapType2 {
+        pname = "subminer";
+        version = "0.17.0";
+        src = pkgs.fetchurl {
+          url = "https://github.com/ksyasuda/SubMiner/releases/download/v0.17.0/SubMiner.AppImage";
+          sha256 = "sha256-aEqhxQ0cFxlJ4Tpsh0foIg4O8ZF9QQzVfSULGxPT+iU=";
+        };
+        extraPkgs = pkgs: with pkgs; [ ];
+      };
+    in {
+      packages.default = subminer;
+
+      apps.default = {
+        type = "app";
+        program = "${subminer}/bin/subminer";
+      };
+    }
+  );
 }
